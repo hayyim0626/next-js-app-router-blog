ca45fb7cdbd9b50d480f7f3741abe553e1f6742b
hayyim0626 <hayyim0626@sandbank.io>
2024-07-30 20:46:12 +0900

    [add]: next config 이미지 s3 url 추가

--- next.config.js
+++ next.config.js
@@ -1,25 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    //  for external Image (https://nextjs.org/docs/pages/api-reference/components/image)
+    domains: [process.env.NEXT_PUBLIC_S3_DOMAIN],
     remotePatterns: [
-      //   {
-      //     protocol: 'https',
-      //     hostname: 'example.com',
-      //     port: '',
-      //     pathname: '/account123/**',
-      //   },
-    ],
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_S3_DOMAIN,
+        pathname: "*.png"
+      },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_S3_DOMAIN,
+        pathname: "*.jpeg"
+      }
+    ]
   },
   // for svg image
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"]
     });
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: "/coin-calculator",
+        destination: "/coinCalculator"
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
