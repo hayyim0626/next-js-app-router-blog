22472af7464e258da32122b8b8899a5882eb93bb
hayyim0626 <h19960626@gmail.com>
2024-09-25 21:35:45 +0900

    [fix]: next.config.js 파일 변경

--- next.config.js
+++ next.config.js
@@ -1,37 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [process.env.NEXT_PUBLIC_S3_DOMAIN],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_S3_DOMAIN,
-        pathname: "*.png"
-      },
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_S3_DOMAIN,
-        pathname: "*.jpeg"
-      }
-    ]
-  },
-  // for svg image
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/coin-calculator",
-        destination: "/coinCalculator"
-      }
-    ];
-  }
+const nextConfig = (phase, { defaultConfig }) => {
+  const env = process.env.NODE_ENV;
+  const s3Domain = process.env.NEXT_PUBLIC_S3_DOMAIN || "";
+  const hostname = new URL(s3Domain).hostname;
+
+  return {
+    images: {
+      domains: [hostname],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: hostname,
+          pathname: "/images/**"
+        }
+      ]
+    },
+    // for svg image
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"]
+      });
+      return config;
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/coin-calculator",
+          destination: "/coinCalculator"
+        }
+      ];
+    }
+  };
 };
 
 module.exports = nextConfig;
