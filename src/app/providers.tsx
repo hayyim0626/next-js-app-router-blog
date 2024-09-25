91e3e05505f5647254acb408774fdbe5791757c3
hayyim0626 <h19960626@gmail.com>
2024-09-25 21:37:31 +0900

    [fix]: Provider 변경

---
+++ src/app/providers.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 60 * 2 * 1000
+      }
+    }
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
+}
